97552bdc532fc040599ea3a94ebba57e8977e079
Matthew Podber <mpodber@yahoo.com>
2018-02-07 11:18:16 -0600

    added delete jobs

---
+++ scripts/s3m2/delete_s3/delete_launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set +x
+set -e
+
+echo "ENV: $1"
+echo "Type: $2"
+
+THEPROFILE=devops
+
+UPLOADLOCATION="s3://jenkins-automation-artifacts-$1-$2/launch/"
+
+echo "Upload Location: $UPLOADLOCATION"
+
+S3_FILES=$(AWS_PROFILE=devops aws s3 ls $UPLOADLOCATION | grep -v PRE | awk '{print $4}')
+
+for S3F in $S3_FILES 
+do
+ AWS_PROFILE=$THEPROFILE aws s3 rm $UPLOADLOCATION$S3F
+done
